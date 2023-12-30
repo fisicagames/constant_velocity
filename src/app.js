c081e27961c3dcf729b31d6df5facbe1b7e38d85
fisicagames <rafaelj3d@gmail.com>
2023-12-30 19:27:04 -0300

    First commit

---
+++ src/app.js
@@ -0,0 +1,51 @@
+import "@babylonjs/inspector";
+import { Engine, Scene, Color4, ArcRotateCamera, Vector3, HemisphericLight, MeshBuilder } from "@babylonjs/core";
+import { AdvancedDynamicTexture } from "@babylonjs/gui";
+class App {
+    constructor() {
+        var canvas = document.createElement("canvas");
+        let screenW = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+        let screenH = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+        if (screenH / screenW < 1.8) {
+            canvas.style.width = "56svh";
+            canvas.style.height = "100svh";
+        }
+        else {
+            canvas.style.width = "98svw";
+            canvas.style.height = "94svh";
+        }
+        console.log(canvas.style.width, canvas.style.height);
+        canvas.id = "gameCanvas";
+        document.body.appendChild(canvas);
+        var engine = new Engine(canvas, true);
+        var scene = new Scene(engine);
+        scene.clearColor = Color4.FromHexString("#1DB9C3");
+        var camera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, Vector3.Zero(), scene);
+        camera.attachControl(canvas, true);
+        camera.position = new Vector3(0, 3, -3);
+        var light1 = new HemisphericLight("light1", new Vector3(1, 1, 0), scene);
+        var plane = MeshBuilder.CreatePlane('plane', { width: 20, height: 10 }, scene);
+        plane.position = new Vector3(0, 0, 0);
+        plane.rotation.x = Math.PI / 2;
+        var cube = MeshBuilder.CreateBox('cube', { width: 5, height: 5, depth: 5 }, scene);
+        let advancedTexture = AdvancedDynamicTexture.CreateFullscreenUI("GUI", true, scene);
+        let loadedGUI = advancedTexture.parseFromURLAsync("./assets/gui/guiTexture.json");
+        window.addEventListener("keydown", (ev) => {
+            if (ev.shiftKey && ev.ctrlKey && ev.altKey && ev.keyCode === 73) {
+                if (scene.debugLayer.isVisible()) {
+                    scene.debugLayer.hide();
+                }
+                else {
+                    scene.debugLayer.show();
+                }
+            }
+        });
+        engine.runRenderLoop(() => {
+            scene.render();
+        });
+        window.addEventListener("resize", function () {
+            engine.resize();
+        });
+    }
+}
+new App();
---
+++ src/app.ts
@@ -0,0 +1,75 @@
+//import "@babylonjs/core/Debug/debugLayer";
+import "@babylonjs/inspector";
+import { Engine, Scene, Color4, ArcRotateCamera, Vector3, HemisphericLight, Mesh, MeshBuilder } from "@babylonjs/core";
+import { AdvancedDynamicTexture } from "@babylonjs/gui";
+
+//https://colorhunt.co/palette/1db9c37027a0c32badf56fad
+//https://gui.babylonjs.com/#HEG7HH#11
+//Mobile simulator - responsive testing tool
+//https://chromewebstore.google.com/detail/mobile-simulator-responsi/ckejmhbmlajgoklhgbapkiccekfoccmk
+
+
+
+class App {
+    constructor() {
+        // create the canvas html element and attach it to the webpage
+        var canvas = document.createElement("canvas");
+
+        let screenW = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+        let screenH = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+        //console.log(screenH, screenW, screenH / screenW);
+        if (screenH / screenW < 1.8) {
+            canvas.style.width = "56svh";
+            canvas.style.height = "100svh"
+        }
+        else {
+            canvas.style.width = "98svw";
+            canvas.style.height = "94svh"
+        }
+
+
+        console.log(canvas.style.width, canvas.style.height);
+        canvas.id = "gameCanvas";
+        document.body.appendChild(canvas);
+
+        // initialize babylon scene and engine
+        var engine = new Engine(canvas, true);
+        var scene = new Scene(engine);
+        scene.clearColor = Color4.FromHexString("#1DB9C3");
+
+
+        var camera: ArcRotateCamera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, Vector3.Zero(), scene);
+        camera.attachControl(canvas, true);
+        camera.position = new Vector3(0,3,-3);
+        var light1: HemisphericLight = new HemisphericLight("light1", new Vector3(1, 1, 0), scene);
+        //var sphere: Mesh = MeshBuilder.CreateSphere("sphere", { diameter: 0.5 }, scene);
+        var plane: Mesh =  MeshBuilder.CreatePlane('plane',{width:20,height:10}, scene);
+        plane.position = new Vector3(0, 0, 0);
+        plane.rotation.x = Math.PI/2;
+        var cube: Mesh = MeshBuilder.CreateBox('cube',{width:5,height:5,depth:5}, scene);
+        let advancedTexture = AdvancedDynamicTexture.CreateFullscreenUI("GUI", true, scene);
+        let loadedGUI = advancedTexture.parseFromURLAsync("./assets/gui/guiTexture.json");
+
+        // hide/show the Inspector
+        window.addEventListener("keydown", (ev) => {
+            // Shift+Ctrl+Alt+I
+            if (ev.shiftKey && ev.ctrlKey && ev.altKey && ev.keyCode === 73) {
+                if (scene.debugLayer.isVisible()) {
+                    scene.debugLayer.hide();
+                } else {
+                    scene.debugLayer.show();
+                }
+            }
+        });
+
+        // run the main render loop
+        engine.runRenderLoop(() => {
+            scene.render();
+        });
+        // Resize
+        window.addEventListener("resize", function () {
+            engine.resize();
+        });
+    }
+}
+new App();
